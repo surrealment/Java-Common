bdc8acd85641a5286c074321a6fe553e8ba2939d
Shockelite <shockelite@surrealment.com>
2018-07-27 11:11:47 -0700

    Moved TryParse methods in seMath to seString. Added random to TryParseFloat.

--- src/com/surrealment/seMath.java
+++ src/com/surrealment/seMath.java
@@ -5,53 +5,6 @@ public class seMath {
     //region byte
 
     /**
-     * Parse string to byte.
-     *
-     * @param s
-     * @return
-     */
-    final public static byte TryParseByte(String s) {
-        s = seString.NullCheck(s);
-        switch (s.toLowerCase()) {
-            case "min":
-                return (Byte.MAX_VALUE * -1 - 1);
-            case "max":
-                return Byte.MAX_VALUE;
-            case "ran":
-                return (byte) seRandom.Range((Byte.MAX_VALUE * -1 - 1), Byte.MAX_VALUE);
-            case "pos":
-                return (byte) seRandom.Range(1, Byte.MAX_VALUE);
-            case "neg":
-                return (byte) seRandom.Range((Byte.MAX_VALUE * -1 - 1), -1);
-            case "":
-            case "null":
-            case "zero":
-                return 0;
-        }
-        try {
-            return Byte.parseByte(s);
-        } catch (NumberFormatException e) {
-            System.out.println("ERROR \"" + s + "\" is not in valid byte data type format!");
-            System.out.println("      Use numbers between " + (Byte.MAX_VALUE * -1 - 1) + " and " + Byte.MAX_VALUE + ".");
-            System.out.println("      Or use keywords min, max or zero.");
-            return 0;
-        }
-    }
-
-    /**
-     * Parse string to byte array.
-     *
-     * @param s
-     * @return
-     */
-    final public static byte[] TryParseByte(String[] s) {
-        byte[] retVal = new byte[s.length];
-        for (int i = 0; i < s.length; i++)
-            retVal[i] = TryParseByte(s[i]);
-        return retVal;
-    }
-
-    /**
      * Limit value to over minimum and under maximum.
      *
      * @param value
@@ -158,53 +111,6 @@ public class seMath {
     //endregion
 
     //region short
-
-    /**
-     * Parse string to short.
-     *
-     * @param s
-     * @return
-     */
-    final public static short TryParseShort(String s) {
-        s = seString.NullCheck(s);
-        switch (s.toLowerCase()) {
-            case "min":
-                return (Short.MAX_VALUE * -1 - 1);
-            case "max":
-                return Short.MAX_VALUE;
-            case "ran":
-                return (short) seRandom.Range((Short.MAX_VALUE * -1 - 1), Short.MAX_VALUE);
-            case "pos":
-                return (short) seRandom.Range(1, Short.MAX_VALUE);
-            case "neg":
-                return (short) seRandom.Range((Short.MAX_VALUE * -1 - 1), -1);
-            case "":
-            case "null":
-            case "zero":
-                return 0;
-        }
-        try {
-            return Short.parseShort(s);
-        } catch (NumberFormatException e) {
-            System.out.println("ERROR \"" + s + "\" is not in valid short data type format!");
-            System.out.println("      Use numbers between " + (Short.MAX_VALUE * -1 - 1) + " and " + Short.MAX_VALUE + ".");
-            System.out.println("      Or use keywords min, max or zero.");
-            return 0;
-        }
-    }
-
-    /**
-     * Parse string to short array.
-     *
-     * @param s
-     * @return
-     */
-    final public static short[] TryParseShort(String[] s) {
-        short[] retVal = new short[s.length];
-        for (int i = 0; i < s.length; i++)
-            retVal[i] = TryParseShort(s[i]);
-        return retVal;
-    }
 
     /**
      * Limit value to over minimum and under maximum.
@@ -327,53 +233,6 @@ public class seMath {
     //endregion
 
     //region int
-
-    /**
-     * Parse string to int.
-     *
-     * @param s
-     * @return
-     */
-    final public static int TryParseInt(String s) {
-        s = seString.NullCheck(s);
-        switch (s.toLowerCase().trim()) {
-            case "min":
-                return (Integer.MAX_VALUE * -1 - 1);
-            case "max":
-                return Integer.MAX_VALUE;
-            case "ran":
-                return seRandom.Range((Integer.MAX_VALUE * -1 - 1), Integer.MAX_VALUE);
-            case "pos":
-                return seRandom.Range(1, Integer.MAX_VALUE);
-            case "neg":
-                return seRandom.Range((Integer.MAX_VALUE * -1 - 1), -1);
-            case "":
-            case "null":
-            case "zero":
-                return 0;
-        }
-        try {
-            return Integer.parseInt(s);
-        } catch (NumberFormatException e) {
-            System.out.println("ERROR \"" + s + "\" is not in valid int data type format!");
-            System.out.println("      Use numbers between " + (Integer.MAX_VALUE * -1 - 1) + " and " + Integer.MAX_VALUE + ".");
-            System.out.println("      Or use keywords min, max or zero.");
-            return 0;
-        }
-    }
-
-    /**
-     * Parse string to int array.
-     *
-     * @param s
-     * @return
-     */
-    final public static int[] TryParseInt(String[] s) {
-        int[] retVal = new int[s.length];
-        for (int i = 0; i < s.length; i++)
-            retVal[i] = TryParseInt(s[i]);
-        return retVal;
-    }
 
     /**
      * Limit value to over minimum and under maximum.
@@ -535,47 +394,6 @@ public class seMath {
     //region long
 
     /**
-     * Parse string to long.
-     *
-     * @param s
-     * @return
-     */
-    final public static long TryParseLong(String s) {
-        s = seString.NullCheck(s);
-        switch (s.toLowerCase()) {
-            case "min":
-                return (Long.MAX_VALUE * -1 - 1);
-            case "max":
-                return Long.MAX_VALUE;
-            case "":
-            case "null":
-            case "zero":
-                return 0l;
-        }
-        try {
-            return Long.parseLong(s);
-        } catch (NumberFormatException e) {
-            System.out.println("ERROR \"" + s + "\" is not in valid long data type format!");
-            System.out.println("      Use numbers between " + (Long.MAX_VALUE * -1 - 1) + " and " + Long.MAX_VALUE + ".");
-            System.out.println("      Or use keywords min, max or zero.");
-            return 0l;
-        }
-    }
-
-    /**
-     * Parse string to long array.
-     *
-     * @param s
-     * @return
-     */
-    final public static long[] TryParseLong(String[] s) {
-        long[] retVal = new long[s.length];
-        for (int i = 0; i < s.length; i++)
-            retVal[i] = TryParseLong(s[i]);
-        return retVal;
-    }
-
-    /**
      * Limit value to over minimum and under maximum.
      *
      * @param value
@@ -714,47 +532,6 @@ public class seMath {
     //region float
 
     /**
-     * Parse string to float.
-     *
-     * @param s
-     * @return
-     */
-    final public static float TryParseFloat(String s) {
-        s = seString.NullCheck(s);
-        switch (s.toLowerCase()) {
-            case "min":
-                return (Float.MAX_VALUE * -1 - 1);
-            case "max":
-                return Float.MAX_VALUE;
-            case "":
-            case "null":
-            case "zero":
-                return 0f;
-        }
-        try {
-            return Float.parseFloat(s);
-        } catch (NumberFormatException e) {
-            System.out.println("ERROR \"" + s + "\" is not in valid float data type format!");
-            System.out.println("      Use numbers between " + (Float.MAX_VALUE * -1 - 1) + " and " + Float.MAX_VALUE + ".");
-            System.out.println("      Or use keywords min, max or zero.");
-            return 0f;
-        }
-    }
-
-    /**
-     * Parse string to float array.
-     *
-     * @param s
-     * @return
-     */
-    final public static float[] TryParseFloat(String[] s) {
-        float[] retVal = new float[s.length];
-        for (int i = 0; i < s.length; i++)
-            retVal[i] = TryParseFloat(s[i]);
-        return retVal;
-    }
-
-    /**
      * Limit value to over minimum and under maximum.
      *
      * @param value
@@ -891,47 +668,6 @@ public class seMath {
     //endregion
 
     //region double
-
-    /**
-     * Parse string to double.
-     *
-     * @param s
-     * @return
-     */
-    final public static double TryParseDouble(String s) {
-        s = seString.NullCheck(s);
-        switch (s.toLowerCase()) {
-            case "min":
-                return (Double.MAX_VALUE * -1 - 1);
-            case "max":
-                return Double.MAX_VALUE;
-            case "":
-            case "null":
-            case "zero":
-                return 0d;
-        }
-        try {
-            return Double.parseDouble(s);
-        } catch (NumberFormatException e) {
-            System.out.println("ERROR \"" + s + "\" is not in valid double data type format!");
-            System.out.println("      Use numbers between " + (Double.MAX_VALUE * -1 - 1) + " and " + Double.MAX_VALUE + ".");
-            System.out.println("      Or use keywords min, max or zero.");
-            return 0d;
-        }
-    }
-
-    /**
-     * Parse string to double array.
-     *
-     * @param s
-     * @return
-     */
-    final public static double[] TryParseDouble(String[] s) {
-        double[] retVal = new double[s.length];
-        for (int i = 0; i < s.length; i++)
-            retVal[i] = TryParseDouble(s[i]);
-        return retVal;
-    }
 
     /**
      * Limit value to over minimum and under maximum.
--- src/com/surrealment/seString.java
+++ src/com/surrealment/seString.java
@@ -3,12 +3,26 @@ package com.surrealment;
 import java.math.BigDecimal;
 
 public class seString {
+
+    /**
+     * Eliminates null strings.
+     *
+     * @param s
+     * @return
+     */
     public static String NullCheck(String s) {
         if (s == null)
             return "";
         else return s;
     }
 
+    /**
+     * Produces string with a duplicated character.
+     *
+     * @param character
+     * @param count
+     * @return
+     */
     public static String ReplicateCharacter(char character, int count) {
         char[] spaces = new char[count];
         for (int i = 0; i < count; i++)
@@ -16,14 +30,27 @@ public class seString {
         return new String(spaces);
     }
 
+    /**
+     * Reformates decimal type.
+     *
+     * @param value
+     * @param decimals
+     * @return
+     */
     public static String Simplify(float value, int decimals) {
         return Simplify(new BigDecimal(value).toPlainString(), decimals);
     }
 
+    /**
+     * Reformates decimal type.
+     *
+     * @param value
+     * @param decimals
+     * @return
+     */
     public static String Simplify(double value, int decimals) {
         return Simplify(new BigDecimal(value).toPlainString(), decimals);
     }
-
 
     /**
      * Format float into common abbreviations.
@@ -93,5 +120,285 @@ public class seString {
         else
             return number;
     }
+
+    //region TryParse Primitives
+
+    /**
+     * Parse string to byte.
+     *
+     * @param s
+     * @return
+     */
+    final public static byte TryParseByte(String s) {
+        s = seString.NullCheck(s);
+        switch (s.toLowerCase()) {
+            case "min":
+                return (Byte.MAX_VALUE * -1 - 1);
+            case "max":
+                return Byte.MAX_VALUE;
+            case "ran":
+                return (byte) seRandom.Range((Byte.MAX_VALUE * -1 - 1), Byte.MAX_VALUE);
+            case "pos":
+                return (byte) seRandom.Range(1, Byte.MAX_VALUE);
+            case "neg":
+                return (byte) seRandom.Range((Byte.MAX_VALUE * -1 - 1), -1);
+            case "":
+            case "null":
+            case "zero":
+                return 0;
+        }
+        try {
+            return Byte.parseByte(s);
+        } catch (NumberFormatException e) {
+            System.out.println("ERROR \"" + s + "\" is not in valid byte data type format!");
+            System.out.println("      Use numbers between " + (Byte.MAX_VALUE * -1 - 1) + " and " + Byte.MAX_VALUE + ".");
+            System.out.println("      Or use keywords min, max or zero.");
+            return 0;
+        }
+    }
+
+    /**
+     * Parse string to byte array.
+     *
+     * @param s
+     * @return
+     */
+    final public static byte[] TryParseByte(String[] s) {
+        byte[] retVal = new byte[s.length];
+        for (int i = 0; i < s.length; i++)
+            retVal[i] = TryParseByte(s[i]);
+        return retVal;
+    }
+
+    /**
+     * Parse string to short.
+     *
+     * @param s
+     * @return
+     */
+    final public static short TryParseShort(String s) {
+        s = seString.NullCheck(s);
+        switch (s.toLowerCase()) {
+            case "min":
+                return (Short.MAX_VALUE * -1 - 1);
+            case "max":
+                return Short.MAX_VALUE;
+            case "ran":
+                return (short) seRandom.Range((Short.MAX_VALUE * -1 - 1), Short.MAX_VALUE);
+            case "pos":
+                return (short) seRandom.Range(1, Short.MAX_VALUE);
+            case "neg":
+                return (short) seRandom.Range((Short.MAX_VALUE * -1 - 1), -1);
+            case "":
+            case "null":
+            case "zero":
+                return 0;
+        }
+        try {
+            return Short.parseShort(s);
+        } catch (NumberFormatException e) {
+            System.out.println("ERROR \"" + s + "\" is not in valid short data type format!");
+            System.out.println("      Use numbers between " + (Short.MAX_VALUE * -1 - 1) + " and " + Short.MAX_VALUE + ".");
+            System.out.println("      Or use keywords min, max or zero.");
+            return 0;
+        }
+    }
+
+    /**
+     * Parse string to short array.
+     *
+     * @param s
+     * @return
+     */
+    final public static short[] TryParseShort(String[] s) {
+        short[] retVal = new short[s.length];
+        for (int i = 0; i < s.length; i++)
+            retVal[i] = TryParseShort(s[i]);
+        return retVal;
+    }
+
+    /**
+     * Parse string to int.
+     *
+     * @param s
+     * @return
+     */
+    final public static int TryParseInt(String s) {
+        s = seString.NullCheck(s);
+        switch (s.toLowerCase().trim()) {
+            case "min":
+                return (Integer.MAX_VALUE * -1 - 1);
+            case "max":
+                return Integer.MAX_VALUE;
+            case "ran":
+                return seRandom.Range((Integer.MAX_VALUE * -1 - 1), Integer.MAX_VALUE);
+            case "pos":
+                return seRandom.Range(1, Integer.MAX_VALUE);
+            case "neg":
+                return seRandom.Range((Integer.MAX_VALUE * -1 - 1), -1);
+            case "":
+            case "null":
+            case "zero":
+                return 0;
+        }
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            System.out.println("ERROR \"" + s + "\" is not in valid int data type format!");
+            System.out.println("      Use numbers between " + (Integer.MAX_VALUE * -1 - 1) + " and " + Integer.MAX_VALUE + ".");
+            System.out.println("      Or use keywords min, max or zero.");
+            return 0;
+        }
+    }
+
+    /**
+     * Parse string to int array.
+     *
+     * @param s
+     * @return
+     */
+    final public static int[] TryParseInt(String[] s) {
+        int[] retVal = new int[s.length];
+        for (int i = 0; i < s.length; i++)
+            retVal[i] = TryParseInt(s[i]);
+        return retVal;
+    }
+
+    /**
+     * Parse string to long.
+     *
+     * @param s
+     * @return
+     */
+    final public static long TryParseLong(String s) {
+        s = seString.NullCheck(s);
+        switch (s.toLowerCase()) {
+            case "min":
+                return (Long.MAX_VALUE * -1 - 1);
+            case "max":
+                return Long.MAX_VALUE;
+            case "ran":
+                return seRandom.Range((Long.MAX_VALUE * -1 - 1), Long.MAX_VALUE);
+            case "pos":
+                return seRandom.Range(1l, Long.MAX_VALUE);
+            case "neg":
+                return seRandom.Range((Long.MAX_VALUE * -1 - 1), -1l);
+            case "":
+            case "null":
+            case "zero":
+                return 0l;
+        }
+        try {
+            return Long.parseLong(s);
+        } catch (NumberFormatException e) {
+            System.out.println("ERROR \"" + s + "\" is not in valid long data type format!");
+            System.out.println("      Use numbers between " + (Long.MAX_VALUE * -1 - 1) + " and " + Long.MAX_VALUE + ".");
+            System.out.println("      Or use keywords min, max or zero.");
+            return 0l;
+        }
+    }
+
+    /**
+     * Parse string to long array.
+     *
+     * @param s
+     * @return
+     */
+    final public static long[] TryParseLong(String[] s) {
+        long[] retVal = new long[s.length];
+        for (int i = 0; i < s.length; i++)
+            retVal[i] = TryParseLong(s[i]);
+        return retVal;
+    }
+
+    /**
+     * Parse string to float.
+     *
+     * @param s
+     * @return
+     */
+    final public static float TryParseFloat(String s) {
+        s = seString.NullCheck(s);
+        switch (s.toLowerCase()) {
+            case "min":
+                return (Float.MAX_VALUE * -1 - 1);
+            case "max":
+                return Float.MAX_VALUE;
+            case "ran":
+                return seRandom.Range((Float.MAX_VALUE * -1f - 1f), Float.MAX_VALUE);
+            case "pos":
+                return seRandom.Range(1f, Float.MAX_VALUE);
+            case "neg":
+                return seRandom.Range((Float.MAX_VALUE * -1f - 1f), -1f);
+            case "":
+            case "null":
+            case "zero":
+                return 0f;
+        }
+        try {
+            return Float.parseFloat(s);
+        } catch (NumberFormatException e) {
+            System.out.println("ERROR \"" + s + "\" is not in valid float data type format!");
+            System.out.println("      Use numbers between " + (Float.MAX_VALUE * -1 - 1) + " and " + Float.MAX_VALUE + ".");
+            System.out.println("      Or use keywords min, max or zero.");
+            return 0f;
+        }
+    }
+
+    /**
+     * Parse string to float array.
+     *
+     * @param s
+     * @return
+     */
+    final public static float[] TryParseFloat(String[] s) {
+        float[] retVal = new float[s.length];
+        for (int i = 0; i < s.length; i++)
+            retVal[i] = TryParseFloat(s[i]);
+        return retVal;
+    }
+
+    /**
+     * Parse string to double.
+     *
+     * @param s
+     * @return
+     */
+    final public static double TryParseDouble(String s) {
+        s = seString.NullCheck(s);
+        switch (s.toLowerCase()) {
+            case "min":
+                return (Double.MAX_VALUE * -1 - 1);
+            case "max":
+                return Double.MAX_VALUE;
+            case "":
+            case "null":
+            case "zero":
+                return 0d;
+        }
+        try {
+            return Double.parseDouble(s);
+        } catch (NumberFormatException e) {
+            System.out.println("ERROR \"" + s + "\" is not in valid double data type format!");
+            System.out.println("      Use numbers between " + (Double.MAX_VALUE * -1 - 1) + " and " + Double.MAX_VALUE + ".");
+            System.out.println("      Or use keywords min, max or zero.");
+            return 0d;
+        }
+    }
+
+    /**
+     * Parse string to double array.
+     *
+     * @param s
+     * @return
+     */
+    final public static double[] TryParseDouble(String[] s) {
+        double[] retVal = new double[s.length];
+        for (int i = 0; i < s.length; i++)
+            retVal[i] = TryParseDouble(s[i]);
+        return retVal;
+    }
+
+    //endregion
 
 }
